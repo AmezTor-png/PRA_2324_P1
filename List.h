f80a595be23fbd7674c0ee6df1204ed4debdf5e3
Iván Tortajada Amézcua <itorame@upv.edu.es>
2025-10-16 17:56:27 +0200

    Implement error handling in insert method
    Added error handling for invalid position in insert method.

--- List.h
+++ List.h
@@ -1,16 +1,55 @@
 #ifndef LIST_H
 #define LIST_H
 
+using namespace std;
+
 class List
 {
 	public:
 		void insert(int pos, T e)
 		{
-			
+			if(pos < 0 || pos > size())
+			{
+				throw out_of_range("Posicion fuera del rango valido.");
+			}
+			else
+			{
+				ListArray.arr[pos] = e;
+			}
 		}
 
 		void append(T e)
 		{
 			
 		}
-}
+
+		void prepend(T e)
+		{
+			
+		}
+
+		T remove(int pos)
+		{
+			
+		}
+
+		T get(int pos)
+		{
+			
+		}
+
+		int search(T e)
+		{
+			
+		}
+
+		bool empty()
+		{
+			
+		}
+
+		int size()
+		{
+			return n;
+		}
+};
