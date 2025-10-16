cbb17cdf75a1af15a3321d86b539612d668ed740
Iván Tortajada Amézcua <itorame@upv.edu.es>
2025-10-16 18:15:58 +0200

    Add ListArray class definition in ListArray.h

---
+++ ListArray.h
@@ -0,0 +1,15 @@
+#include <ostream>
+#include "List.h"
+
+using namespace std;
+
+template <typename T> 
+class ListArray : public List<T> {
+
+    private:
+        int 
+
+    public:
+        // miembros públicos, incluidos los heredados de List<T>
+    
+};
